8c2cffb2f65c67c7501e444361997c001168b0c8
abmarghoub <a.marghoub5257@uca.ac.ma>
2025-12-24 06:40:09 +0100

    miniprojet 2

---
+++ miniprojet2/microservices-messaging-producer/src/main/java/ma/ens/producer/MicroservicesMessagingProducerApplication.java
@@ -0,0 +1,13 @@
+package ma.ens.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesMessagingProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesMessagingProducerApplication.class, args);
+	}
+
+}
